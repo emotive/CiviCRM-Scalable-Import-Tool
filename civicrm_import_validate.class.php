18b99cb4cb8b2f10e60f9b751dfe48dec8974139
Chang Xiao <chang@emotivellc.com>
2010-06-07 08:48:50 -0400

    Master Branch Commit

---
+++ civicrm_import_validate.class.php
@@ -0,0 +1,323 @@
+<?php
+
+require_once('lib/parsecsv.class.php');
+require_once('civicrm_import_util.class.php');
+require_once('civicrm_import_db.class.php');
+
+/*
+ * Import Validation Class
+ */
+class civicrm_import_validate extends civicrm_import_db {
+	
+	private $csv;
+	private $custom_field_errors;
+	public $log;
+	
+	public function __construct($db = array(), $logging = array()) {
+		$this->csv = new parseCSV();
+		$this->csv->heading = FALSE;
+		
+		// the logger file name should be consistent with logger in
+		// contact_importer.php otherwise it would be confusing
+		$toggle = ($logging == 1) ? TRUE : FALSE;
+		$this->log = new civicrm_import_utils($logging['path'], $toggle);
+		
+		// invoke the parent and establish db connection
+		parent::__construct($db['host'], $db['name'], $db['user'], $db['pass']);
+	}
+	
+	public function __get($variable) {
+		return $this->$variable;
+	}
+	
+	public function __set($variable, $value) {
+		$this->$variable = $value;
+	}
+	
+	/*******************************************************************************************************************
+	 * Validate Standard Import Fields (Email, States, Zip Codes)
+	 * @param
+	 * mapping [array]			|	key:	csv column		value: field name
+	 * csv_filepath [string]	|							value: file path of the csv file
+	 * depth [int]				|							value: number of rows to scan in the csv file
+	 * csv_log [bool]			|							value: TRUE | FALSE  (whether log the error into csv or not)
+	 *
+	 * @returns
+	 * data [array]				|	key:	status			value: 0 | 1 (fail, pass)
+	 * 							|	key:	[errors]		value: empty | [array]
+	 *							|	key:	report			value: report file path
+	 */	
+	public function validate_fields($_mapping = array(), $csv_filepath, $depth = 100, $csv_log = FALSE) {
+		
+		// set loggin
+		$this->log->__set('logging', $csv_log);
+		
+		$mapping = array();
+		
+		#1 Extract the standard fields that we are checking from the raw mapping array
+		foreach($_mapping as $column => $field) {
+			if($field == 'email') {
+				$mapping[$column] = $field;
+			}
+			if(strstr($field, 'state_province')) {
+				$mapping[$column] = $field;
+			}
+			if(strstr($field, 'postal_code')) {
+				$mapping[$column] = $field;
+			}
+		}
+		
+		#2 Parse the first x number of records
+		$this->csv->offset = 1;
+		$this->csv->limit = $depth;
+		$this->csv->auto($csv_filepath);
+		
+		$return_data = array();
+		
+		$invalid = array(
+			'email' => 0,
+			'state' => 0,
+			'zip' => 0,
+		);
+		
+		foreach($mapping as $column => $field) {
+			for($i = 0; $i<count($this->csv->data); $i++) {
+				if($field == 'email') {
+					if(!$this->valid_email($this->csv->data[$i][$column])) {
+						$invalid['email']++;
+						// add the record to the csv
+						$this->csv->data[$i][$column] .= ' [invalid email]';
+						$this->log->_log($this->csv->unparse(array($this->csv->data[$i])), 'error_csv', FALSE);
+					}
+				}
+				if(strstr($field, 'state_province')) {
+					if(!$this->valid_state($this->csv->data[$i][$column])) {
+						$invalid['state']++;
+						// add the record to the csv	
+						$this->csv->data[$i][$column] .= ' [invalid state/province]';
+						$this->log->_log($this->csv->unparse(array($this->csv->data[$i])), 'error_csv', FALSE);						
+					}
+				}
+				if(strstr($field, 'postal_code')) {
+					if(!$this->valid_zip($this->csv->data[$i][$column])) {
+						$invalid['zip']++;
+						// add the record to the csv
+						$this->csv->data[$i][$column] .= ' [invalid zip code]';
+						$this->log->_log($this->csv->unparse(array($this->csv->data[$i])), 'error_csv', FALSE);				
+					}
+				}
+			}
+		}
+		
+		if(array_sum($invalid) == 0) {
+			$return_data['status'] = 1;
+		} else {
+			$return_data['status'] = 0;
+			
+			if($invalid['email'] > 0) {
+				$return_data['errors']['email'] = $invalid['email'];
+			}
+			if($invalid['state'] > 0) {
+				$return_data['errors']['state'] = $invalid['state'];
+			}
+			if($invalid['zip'] > 0) {
+				$return_data['errors']['zip'] = $invalid['zip'];
+			}
+		}
+		
+		// kill validation data container
+		unset($this->csv->data);
+		
+		return $return_data;
+	}
+	
+	/*******************************************************************************************************************
+	 * Validate SET custom field values (CheckBox, Select, Radio) against their values from the DB
+	 * @param
+	 * mapping [array]			|	key:	csv column		value: field name
+	 * csv_filepath [string]	|							value: file path of the csv file
+	 * depth [int]				|							value: number of rows to scan in the csv file
+	 * csv_log [bool]			|							value: TRUE | FALSE  (whether log the error into csv or not)
+	 *
+	 * @returns
+	 * 
+	 * TRUE						|	Passes SET value custom field validation or no SET VALUE custom field detected
+	 * FALSE					|	Fails custom field validation
+	 * $this->custom_field_errors (internal)
+	 */	
+	public function validate_custom_fields($mapping = array(), $csv_filepath, $depth = 100, $csv_log = FALSE) {
+		
+		$this->log->__set('logging', $csv_log);
+		
+		# 1st, get the import data to see if there's any custom fields 
+		// $mappings = unserialize($this->data->mapping);
+		$custom_fields_mapping = array();
+		$custom_fields_mapping_set = array();
+		
+		foreach($mapping as $column => $field) {
+			if(strstr($field, 'custom_')) {
+				$custom_fields_mapping[$column] = substr($field, 7, 8); // just want the custom field id
+			}
+		}
+		
+		// if there's no custom fields to import, don't waste time
+		if(!empty($custom_fields_mapping)) {
+		
+			# 2nd, are they radios, checkboxes or selections? (SET values)
+			foreach($custom_fields_mapping as $column => $custom_field_id) {
+				
+				$custom_field_type = $this->db->get_var(sprintf("SELECT html_type FROM civicrm_custom_field WHERE id = %d", $custom_field_id));
+					
+					if($custom_field_type == 'Select' || $custom_field_type == 'CheckBox' || $custom_field_type == 'Radio') {
+						$custom_fields_mapping_set[$column] = $custom_field_id;
+					}
+			}
+			
+			// only care if the custom field we import has set values
+			if(!empty($custom_fields_mapping_set)) {
+			
+				# 3rd, fetch the expected set values of the custom fields
+				$errors = 0;
+				foreach($custom_fields_mapping_set as $column => $custom_field_id) {
+				
+					$query = sprintf("SELECT cov.value FROM civicrm_option_value cov 
+					JOIN civicrm_option_group cog ON cov.option_group_id = cog.id
+					JOIN civicrm_custom_field ccf ON ccf.option_group_id = cog.id
+					WHERE ccf.option_group_id IS NOT NULL AND ccf.id = %d", $custom_field_id);
+					
+					if($results = $this->db->get_results($query, ARRAY_N)) {
+					
+						# 4 match the fields with csv data
+						$_res = civicrm_import_utils::flattenArray($results);
+						
+						#4.1 parse limited data
+						$this->csv->offset = 1;
+						$this->csv->limit = $depth;
+						$this->csv->parse($csv_filepath);
+						
+						
+						for($i = 0; $i<count($this->csv->data); $i++) {
+							if(!in_array($this->csv->data[$i][$column], $_res)) {
+								$errors++;
+								// mark the error and log the data validation error
+								$this->csv->data[$i][$column] .= ' [acceptable values are: ' . implode(',', $_res) . ']';
+								$this->log->_log($this->csv->unparse(array($this->csv->data[$i])), 'error_csv', FALSE);
+							}
+						}
+					}
+					
+				} // end of foreach
+			
+				if($errors > 0) {
+					// Trigger error email and point or attach the error csv report and include the message below
+					// echo $errors . " Error(s) occured. Inconsistent custom data detected, this import job is cancelled";
+					$this->custom_field_errors = $errors;
+					return FALSE;	
+				}
+				
+				// At this point we are successful?
+				unset($this->csv->data);
+				
+				return TRUE;
+			}
+		
+		} else {
+			// if no custom field detected, just return TRUE
+			return TRUE;
+		}
+	} // end of validate custom field
+	
+	public static function valid_state($state_input) {
+	
+		$states_short = array('AL','AK','AS','AZ','AR','CA','CO','CT','DE','DC','FM','FL','GA','GU','HI','ID','IL','IN','IA','KS','KY','LA','ME','MH','MD','MA','MI','MN','MS','MO','MT','NE','NV','NH','NJ','NM','NY','NC','ND','MP','OH','OK','OR','PW','PA','PR','RI','SC','SD','TN','TX','UT','VT','VI','VA','WA','WV','WI','WY');
+		
+		$states_full = array('ALABAMA','ALASKA','AMERICAN SAMOA','ARIZONA','ARKANSAS','CALIFORNIA','COLORADO','CONNECTICUT','DELAWARE','DISTRICT OF COLUMBIA','FEDERATED STATES OF MICRONESIA','FLORIDA','GEORGIA','GUAM','HAWAII','IDAHO','ILLINOIS','INDIANA','IOWA','KANSAS','KENTUCKY','LOUISIANA','MAINE','MARSHALL ISLANDS','MARYLAND','MASSACHUSETTS','MICHIGAN','MINNESOTA','MISSISSIPPI','MISSOURI','MONTANA','NEBRASKA','NEVADA','NEW HAMPSHIRE','NEW JERSEY','NEW MEXICO','NEW YORK','NORTH CAROLINA','NORTH DAKOTA','NORTHERN MARIANA ISLANDS','OHIO','OKLAHOMA','OREGON','PALAU','PENNSYLVANIA','PUERTO RICO','RHODE ISLAND','SOUTH CAROLINA','SOUTH DAKOTA','TENNESSEE','TEXAS','UTAH','VERMONT','VIRGIN ISLANDS','VIRGINIA','WASHINGTON','WEST VIRGINIA','WISCONSIN','WYOMING');
+		
+		// string less than 2 chars are bad!
+		if(strlen($state_input) < 2) {
+			return FALSE;
+		
+		// check abbr version of the states
+		} elseif(strlen($state_input) == 2) {
+			
+			if(in_array(strtoupper($state_input), $states_short)) {
+				return TRUE;
+			} else {
+				return FALSE;
+			}
+		}
+		// check full version of the states
+		else {
+		
+			if(in_array(strtoupper($state_input), $states_full)) {
+				return TRUE;
+			} else {
+				return FALSE;
+			}
+		}
+	}
+	
+	// can't check for string length because sometimes
+	// it is presented in xxxxx-xxxx
+	public static function valid_zip($zip_code) {
+		if(!is_numeric($zip_code)) {
+			return FALSE;
+		} else {
+			return TRUE;
+		}
+	}
+	
+	public static function valid_email($email) {
+	   $isValid = true;
+	   $atIndex = strrpos($email, "@");
+	   if (is_bool($atIndex) && !$atIndex)
+	   {
+		  $isValid = false;
+	   }
+	   else {
+		  $domain = substr($email, $atIndex+1);
+		  $local = substr($email, 0, $atIndex);
+		  $localLen = strlen($local);
+		  $domainLen = strlen($domain);
+		  if ($localLen < 1 || $localLen > 64)
+		  {
+			 // local part length exceeded
+			 $isValid = false;
+		  }
+		  else if ($domainLen < 1 || $domainLen > 255) {
+			 // domain part length exceeded
+			 $isValid = false;
+		  }
+		  else if ($local[0] == '.' || $local[$localLen-1] == '.') {
+			 // local part starts or ends with '.'
+			 $isValid = false;
+		  }
+		  else if (preg_match('/\\.\\./', $local)) {
+			 // local part has two consecutive dots
+			 $isValid = false;
+		  }
+		  else if (!preg_match('/^[A-Za-z0-9\\-\\.]+$/', $domain)) {
+			 // character not valid in domain part
+			 $isValid = false;
+		  }
+		  else if (preg_match('/\\.\\./', $domain)) {
+			 // domain part has two consecutive dots
+			 $isValid = false;
+		  }
+		  else if(!preg_match('/^(\\\\.|[A-Za-z0-9!#%&`_=\\/$\'*+?^{}|~.-])+$/', str_replace("\\\\","",$local))) {
+			 // character not valid in local part unless 
+			 // local part is quoted
+			 if (!preg_match('/^"(\\\\"|[^"])+"$/',
+				 str_replace("\\\\","",$local)))
+			 {
+				$isValid = false;
+			 }
+		  }
+	   }
+	   return $isValid;	
+	}
+
+} // end of class
+
+
+?>
---
+++ readme.txt
@@ -0,0 +1,133 @@
+/*****************************************
+ * CiviCRM Scalable Import Module
+ * by Chang Xiao (chang@emotivellc.com)
+ * EMotive LLC, 2010
+ * GNU GENERAL LICENSE - see LICENSE.txt
+ ****************************************/
+ 
+1. Requirements
+2. Installation
+3. Configuration 
+4. Usage
+5. Known Issues
+6. Future considerations
+7. Change log
+
+/*************************************************************
+ * 1. Requirements
+ ************************************************************/
+ 
+This module requires the following:
+ 
+	1. Drupal Version 6.x
+	2. CiviCRM 3.x (Versions with API V2)
+	3. *Your Drupal and CiviCRM needs to be in the same database*
+	3. Recommended of PHP memory limit control (either through .htaccess or ini_set())
+	4. 512 MB PHP memory allocation to avoid memory exhaustion error
+	5. Drupal upload element module
+	6. (Optional) CURL installed [on linux machines]
+
+/*************************************************************
+ * 2. Installation 
+ ************************************************************/	
+
+ The following steps are needed to make the module work:
+ 
+	1. Upload all the contents in the civicrm_import to your drupal 
+	sites/all/modules/ folder.
+	
+	2. Enable the module "CiviCRM Mass Import" from admin/build/modules
+	
+	3. Configurate the module from admin/civi_import/config
+	
+	4. Change the following line in *civicrm_importer.class.php*
+	* chdir('path/to/drupal_install');*
+	This should be changed to the *full path* of your drupal installation such as 
+	chdir('/var/www'), etc.
+	
+	5. Change the following line in *civicrm_import.cron.php*
+	* chdir('path/to/drupal_install');*
+	This should be changed to the *full path* of your drupal installation such as 
+	chdir('/var/www'), etc.
+
+/*************************************************************
+ * 3. Configuration
+ ************************************************************/		
+
+	1. CMS/CRM Database Host: The host address of your Drupal/CiviCRM
+	(It can be either the ip address or resolvable domain names)
+	
+	2. CMS/CRM Database Name: The name of the database that Drupal/CiviCRM
+	lives
+	
+	3. CMS/CRM Database Username: Username for database access
+	
+	4. CMS/CRM Database Password: Password for database access
+	
+	5. Logging Toggle: Select Yes or No for error and import file logging
+	
+	6. Log PATH: if Logging is selected, the full path to where the logging file will be stored
+	(This should be the full path to /sites/default/files/civicrm_import)
+ 
+	7. Email to: Email status and update that should be sent to
+	
+	8. Email greeting: The name of the email: i.e. John Doe
+	
+	9. CMS db prefix: The Drupal database prefix, (should include the underscore _)
+	
+	10. Import file split lines count: If you have a large file (say 250,000) records and
+	set the line count to 50,000, the script will split the file into 5 parts and attempt
+	to import through each file. This will prevent timeout and other PHP issues and
+	improve import availability and job scheduling.
+
+/*************************************************************
+ * 4. Usage
+ ************************************************************/	
+
+	* civicrm_import.cron.php * should be ran by the cron on 
+	a 5 minutes basis or longer interval. If running the file
+	results in php memory exhaustion error, please run
+	cron.php which uses shell_exec and CURL command, you will 
+	need to have curl installed on the server.
+ 
+	1. Browse a CSV file to import, (Recommend with column headers)
+	2. Select Mapping options (First name and Email address are required import fields)
+	3. Select De-Dupe, Date Format Options
+	4. Select grouping/tagging options
+	5. Check quick validation report, Import
+	
+/*************************************************************
+ * 5. Known Issues
+ ************************************************************/
+	
+	1. The script will look at blank email, states/province and postal codes as 
+	   invalid fields.
+
+	2. The script will only validate email, states/province and postal codes 
+	 
+	3. The quick validation processing only look at the first 100 columns of the
+	   import file
+	
+	4. The script will not format custom fields that are date type, so to import 
+	   those fields, they must be in yyyy-mm-dd in order to be properly imported
+	   
+/*************************************************************
+ * 6. Future Consideration
+ ************************************************************/
+ 
+	1.	Further Decoupling of processes (Email, Validation, Logging)
+	2.	Use Drupal/CiviCRM native database resources (drupal db bootstrap, CiviCRM Dao)
+		to perform SQL queries and making CMS/CRM database independent
+	3.	Use CiviCRM import API to perform import rather than using the Contact/Location/Grouping/Tagging
+	4.	More field level validation for different type of fields.
+	5.	Validation options and rules for importing (so it will only import records that meets
+		certain criteria
+	
+	If you have other ideas and suggestions, please contact at chang@emotivellc.com
+	and feel free to contribute to the code
+	
+/*************************************************************
+ * 7. Change log
+ ************************************************************/
+ 
+ Alpha 1 release: 6/5/2010
